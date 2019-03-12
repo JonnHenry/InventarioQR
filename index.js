dba3912043291e2b9d148d8f6b12803779379ef1
Jonnathan Campoberde <jonnathanca9601@gmail.com>
2019-03-11 22:44:26 -0500

    Arreglo en la api

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/inventario/nuevo', (req, res) => { // Para poder crear un inventario
   }).spread((result, created) => { // Si este fue encontrado retorna un booleano con verdadero si el objeto fue creado
     if (created) {
       res.json({
-        respuesta: 'El inventario tiene el codigo:' + result.codigoInventario
+        respuesta: 'El fue agregado con exito'
       })
     } else {
       res.json({
